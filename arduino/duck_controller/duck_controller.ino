771d837a1b731e5590d12365667b681f5ce4706c
Patrick Martin <pux1987@gmail.com>
2018-10-15 23:38:59 -0600

    Created the duck controller program

---
+++ arduino/duck_controller/duck_controller.ino
@@ -0,0 +1,109 @@
+#include <Adafruit_NeoPixel.h>
+
+const int analogReadPin = 0;
+
+const int pixelPin = 6;
+const int ledPin = 13;
+
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, pixelPin, NEO_GRB + NEO_KHZ800);
+
+enum SerialState {
+    WaitingStart,
+    GotC,
+    ParsingRed,
+    ParsingGreen,
+    ParsingBlue,
+};
+
+SerialState state = SerialState::WaitingStart;
+
+int red = 0;
+int green = 0;
+int blue = 0;
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+    pixel.begin();
+    pixel.show();
+}
+
+bool isDigit(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+int asciiToIntValue(char c)
+{
+    return c - '0';
+}
+
+/*!
+ * This looks for a command in the form:
+ * c:<digit>,<digit>,<digit>
+ * and tries to set a neopixel to that color when found
+ * 
+ * example:
+ * c:255,128,0
+ */
+void parseCharacter(char character)
+{
+    switch (state) {
+    case SerialState::WaitingStart:
+        if (character == 'c') {
+            state = SerialState::GotC;
+        }
+        break;
+    case SerialState::GotC:
+        if (character == ':') {
+            state = SerialState::ParsingRed;
+            red = 0;
+        } else {
+            state = SerialState::WaitingStart;
+        }
+        break;
+    case SerialState::ParsingRed:
+        if (isDigit(character)) {
+            red = (red * 10) + asciiToIntValue(character);
+        } else if (character == ',') {
+            state = SerialState::ParsingGreen;
+            green = 0;
+        } else {
+            state = SerialState::WaitingStart;
+        }
+        break;
+    case SerialState::ParsingGreen:
+        if (isDigit(character)) {
+            green = (green * 10) + asciiToIntValue(character);
+        } else if (character == ',') {
+            state = SerialState::ParsingBlue;
+            blue = 0;
+        } else {
+            state = SerialState::WaitingStart;
+        }
+        break;
+    case SerialState::ParsingBlue:
+        if (isDigit(character)) {
+            blue = (blue * 10) + asciiToIntValue(character);
+            break;
+        }
+        if (character == '\n') {
+            pixel.setPixelColor(0, pixel.Color(red, green, blue));
+            pixel.show();
+        }
+        state = SerialState::WaitingStart;
+        break;
+    }
+}
+
+void loop()
+{
+    int reading = analogRead(analogReadPin);
+    Serial.print("r:");
+    Serial.println(reading);
+
+    while (Serial.available()) {
+        parseCharacter(Serial.read());
+    }
+}
