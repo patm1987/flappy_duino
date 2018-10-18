fcc70b724bb5210688fadb909d1bd47cf731d212
Patrick Martin <pux1987@gmail.com>
2018-10-17 21:16:07 -0600

    Created a basic server

--- websocket_server/server.py
+++ websocket_server/server.py
@@ -1,19 +1,40 @@
 import asyncio
+from threading import Thread
+
+import serial
 import websockets
+
+SERIAL_PORT = 'COM8'
+BAUD_RATE = 9600
+
+clients = []
+
+ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
 
 
 @asyncio.coroutine
-def hello(websocket, path):
-    name = yield from websocket.recv()
-    print("< {}".format(name))
+async def serial_server(websocket, path):
+    clients.append(websocket)
+    try:
+        while True:
+            message = await websocket.recv()
+            print("Received {}".format(message))
+    finally:
+        clients.remove(websocket)
 
-    greeting = "hello {}".format(name)
 
-    yield from websocket.send(greeting)
-    print("> {}".format(greeting))
+def read_serial():
+    while ser.isOpen():
+        print("Received: {}".format(ser.readline()))
 
 
-start_server = websockets.serve(hello, port=8765)
+thread = Thread(target=read_serial)
+thread.start()
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+loop = asyncio.get_event_loop()
+start_server = websockets.serve(serial_server, port=8765)
+loop.run_until_complete(start_server)
+loop.run_forever()
+
+ser.close()
+thread.join()
