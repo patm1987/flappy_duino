535cba9ba69c975ddae79954984e41b7f114bb11
Patrick Martin <pux1987@gmail.com>
2018-10-28 12:06:05 -0600

    moved color setting to be more abstracted to help facilitate the demo

---
+++ AssetsForDemo/FlappyDuinoDemo/Assets/Scripts/SendColorFromBoolean.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class SendColorFromBoolean : MonoBehaviour
+{
+    [SerializeField] private Color _trueColor = Color.green;
+    [SerializeField] private Color _falseColor = Color.red;
+    public ColorChangedEvent OnColorChanged = new ColorChangedEvent();
+
+    private bool _value;
+    public bool Value
+    {
+        get { return _value; }
+        set
+        {
+            if (_value != value)
+            {
+                _value = value;
+                Color = _value ? _trueColor : _falseColor;
+            }
+        }
+    }
+
+    private Color _color;
+    public Color Color
+    {
+        get { return _color; }
+        private set
+        {
+            if (_color != value)
+            {
+                _color = value;
+                OnColorChanged.Invoke(_color);
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class ColorChangedEvent : UnityEvent<Color>
+    {
+    }
+}
